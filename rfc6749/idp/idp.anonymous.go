2b672b1034f6dd8d1ac58235c2d3c151464c70a0
Evert Lammerts <evert@lucipher.net>
2017-08-22 22:40:48 +0200

    anonymous idp

---
+++ rfc6749/idp/idp.anonymous.go
@@ -0,0 +1,26 @@
+package idp
+
+import (
+	"errors"
+	"net/http"
+	"net/url"
+
+	"github.com/DatapuntAmsterdam/goauth2/rfc6749/transientstorage"
+)
+
+type AnonymousIdP struct{}
+
+func (a *AnonymousIdP) AuthnRedirect(opaqueToken string, callbackURL url.URL, kv transientstorage.TransientStorageIdP) (string, error) {
+	query := callbackURL.Query()
+	query.Set("token", opaqueToken)
+	callbackURL.RawQuery = query.Encode()
+	return callbackURL.String(), nil
+}
+
+// User returns a User and the original opaque token.
+func (a *AnonymousIdP) User(r *http.Request, kv transientstorage.TransientStorageIdP) (*User, string, error) {
+	if token, ok := r.URL.Query()["token"]; ok {
+		return &User{"Anonymous", []string{}}, token[0], nil
+	}
+	return nil, "", errors.New("No token supplied")
+}
