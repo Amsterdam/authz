139f45f00229054ef43e206e36334e4b25d11aa4
Evert Lammerts <evert@lucipher.net>
2017-09-14 11:13:00 +0200

    accesstoken tests

---
+++ server/accesstoken_test.go
@@ -0,0 +1,87 @@
+package server
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"strings"
+	"testing"
+)
+
+var enc *accessTokenEncoder
+
+func init() {
+	enc = newAccessTokenEncoder([]byte("test"), 60, "issuer")
+}
+
+func decodeJWT(jwt string) (*accessTokenJWTHeader, *accessTokenJWTPayload, error) {
+	var (
+		header  accessTokenJWTHeader
+		payload accessTokenJWTPayload
+	)
+	parts := strings.Split(jwt, ".")
+	if len(parts) != 3 {
+		return nil, nil, fmt.Errorf("JWT shoud have 3 parts, has %d: ", len(parts), jwt)
+	}
+	b64header, b64payload, b64digest := parts[0], parts[1], parts[2]
+	mac := hmac.New(sha256.New, enc.secret)
+	mac.Write([]byte(fmt.Sprintf("%s.%s", b64header, b64payload)))
+	computedB64digest := base64.RawURLEncoding.EncodeToString(mac.Sum(nil))
+	if b64digest != computedB64digest {
+		return nil, nil, fmt.Errorf(
+			"Can't verify accesstoken signature (got: %s, computed: %s)",
+			b64digest, computedB64digest,
+		)
+	}
+	rawHeader, err := base64.RawURLEncoding.DecodeString(b64header)
+	if err != nil {
+		return nil, nil, err
+	}
+	if err := json.Unmarshal(rawHeader, &header); err != nil {
+		return nil, nil, err
+	}
+	rawPayload, err := base64.RawURLEncoding.DecodeString(b64payload)
+	if err != nil {
+		return nil, nil, err
+	}
+	if err := json.Unmarshal(rawPayload, &payload); err != nil {
+		return nil, nil, err
+	}
+	return &header, &payload, nil
+}
+
+func TestEncode(t *testing.T) {
+	var (
+		subject = "subject"
+		scopes  = []string{"scope1", "scope2"}
+	)
+	jwt, err := enc.Encode(subject, scopes)
+	if err != nil {
+		t.Fatal(err)
+	}
+	header, payload, err := decodeJWT(jwt)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if payload.Subject != subject {
+		t.Fatalf("JWT subject doesn't match (expected: %s, got %s)", subject, payload.Subject)
+	}
+	for i, scope := range scopes {
+		if payload.Scopes[i] != scope {
+			t.Fatalf("Scopes dont match (expected: %s, got %s)", scope, payload.Scopes[i])
+		}
+	}
+	if jwt2, err := enc.jwt(header, payload); err != nil {
+		t.Fatal(err)
+	} else if jwt2 != jwt {
+		t.Fatalf("Re-computed JWT doesnt match original (original: %s, computed: %s)", jwt, jwt2)
+	}
+}
+
+func BenchmarkEncode(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		enc.Encode("subject", []string{"abc", "def"})
+	}
+}
