4083bd2a42823a4879713a2e446bbf9e77030c86
Evert Lammerts <evert@lucipher.net>
2017-09-16 20:43:53 +0200

    setup idp handler tests

--- server/handler_idp_test.go
+++ server/handler_idp_test.go
@@ -1,6 +1,30 @@
 package server
 
-import "testing"
+import (
+	"net/url"
+	"testing"
+)
+
+var idProviderHandler *idpHandler
+
+func init() {
+	baseURL, _ := url.Parse("http://testserver/idp")
+
+	idProviderHandler = &idpHandler{baseHandler(), testIdProvider(), baseURL, accessTokenEnc()}
+}
+
+type testAuthzState struct {
+	*authorizationState
+}
+
+func (s *testAuthzState) Do() error {
+	token := "test"
+	store := idProviderHandler.stateStore
+	if err := store.persist(token, s.authorizationState); err != nil {
+		return err
+	}
+	return nil
+}
 
 func TestIdPHandler(t *testing.T) {
 }
